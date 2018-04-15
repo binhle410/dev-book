2539840c469678bc06148dc9f8dbb359b458431c
Peter Pierre <binh@sunrise.vn>
2018-04-15 19:49:38 +0700

    BookBundle

--- config/bundles.php
+++ config/bundles.php
@@ -16,4 +16,5 @@ return [
     Symfony\Bundle\WebServerBundle\WebServerBundle::class => ['dev' => true],
     Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
     ApiPlatform\Core\Bridge\Symfony\Bundle\ApiPlatformBundle::class => ['all' => true],
+	Bean\Bundle\BookBundle\BeanBookBundle::class => ['all' => true]
 ];
