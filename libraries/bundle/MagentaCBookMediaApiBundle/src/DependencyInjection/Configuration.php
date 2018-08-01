762ca25d86fec9698717073508b40d1d9cba378f
Peter Pierre <binh@sunrise.vn>
2018-08-01 13:30:06 +0700

    #        - { path: ^/api, roles: IS_AUTHENTICATED_ANONYMOUSLY, ips: ['%env(DEV_IP)%'] }         - { path: ^/api, roles: IS_AUTHENTICATED_ANONYMOUSLY } #        - { path: ^/api,       roles: IS_AUTHENTICATED_FULLY }
            # ...
            - { path: ^/api/token/refresh, roles: IS_AUTHENTICATED_ANONYMOUSLY }

--- libraries/bundle/MagentaCBookMediaApiBundle/src/DependencyInjection/Configuration.php
+++ libraries/bundle/MagentaCBookMediaApiBundle/src/DependencyInjection/Configuration.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Magenta\Bundle\CBookApiBundle\DependencyInjection;
+namespace Magenta\Bundle\CBookMediaApiBundle\DependencyInjection;
 
 use Symfony\Component\Config\Definition\Builder\TreeBuilder;
 use Symfony\Component\Config\Definition\ConfigurationInterface;
@@ -8,7 +8,7 @@ use Symfony\Component\Config\Definition\ConfigurationInterface;
 class Configuration implements ConfigurationInterface {
 	public function getConfigTreeBuilder() {
 		$treeBuilder = new TreeBuilder();
-		$rootNode    = $treeBuilder->root('magenta_cbook_api');
+		$rootNode    = $treeBuilder->root('magenta_cbook_media_api');
 		
 		$rootNode
 			->children()
