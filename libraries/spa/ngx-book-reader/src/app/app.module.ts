1f3e55f296f3d61b889566f59541f59e63dcfe93
nguyenhongphat0 <hongphat.js@gmail.com>
2018-08-15 17:17:30 +0700

    edit chapter content

--- libraries/spa/ngx-book-reader/src/app/app.module.ts
+++ libraries/spa/ngx-book-reader/src/app/app.module.ts
@@ -6,12 +6,13 @@ import { ScrollSpyIndexModule } from 'ngx-scrollspy';
 import { ScrollSpyAffixModule } from 'ngx-scrollspy';
 
 import { NgModule, NO_ERRORS_SCHEMA } from '@angular/core';
-import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { EditorModule } from "@tinymce/tinymce-angular";
 
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { AppRoutingModule } from './app-routing.module';
-import {BookListingComponent} from './book-listing/book-listing.component';
+import { BookListingComponent } from './book-listing/book-listing.component';
 import { BookReaderComponent } from './book-reader/book-reader.component';
 import { BookModalTocComponent } from './book-modal-toc/book-modal-toc.component';
 import { BookContactComponent } from './book-contact/book-contact.component';
@@ -38,7 +39,8 @@ import { HttpClientModule } from '@angular/common/http';
     ScrollSpyAffixModule,
     HttpClientModule,
     AppRoutingModule,
-    NgbModule.forRoot()
+    NgbModule.forRoot(),
+    EditorModule
   ],
   providers: [],
   bootstrap: [AppComponent]
