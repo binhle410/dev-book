52eea271cd183b566c425e4f98e335a52cbbcdff
nguyenhongphat0 <hongphat.js@gmail.com>
2018-09-04 17:14:50 +0700

    rebuilt with jquery

--- webpack.config.js
+++ webpack.config.js
@@ -7,18 +7,21 @@ Encore
     .setPublicPath('/build')
     .cleanupOutputBeforeBuild()
     .enableSourceMaps(!Encore.isProduction())
+    .enableSassLoader()
     // uncomment to create hashed filenames (e.g. app.abc123.css)
     // .enableVersioning(Encore.isProduction())
 
     // uncomment to define the assets of the project
     // .addEntry('js/app', './assets/js/app.js')
-    // .addStyleEntry('css/app', './assets/css/app.scss')
+    .createSharedEntry('app', './libraries/component/CBookAsset/src/js/app.js')
+    .addStyleEntry('css/app', './libraries/component/CBookAsset/src/css/app.scss')
 
     // uncomment if you use Sass/SCSS files
     // .enableSassLoader()
 
     // uncomment for legacy applications that require $/jQuery as a global variable
     // .autoProvidejQuery()
+    .enableVueLoader()
 ;
 
 module.exports = Encore.getWebpackConfig();
