5c66cc04a5ec90733ec9d226ca07cd196031f6a2
nguyenhongphat0 <hongphat.js@gmail.com>
2018-09-06 15:27:15 +0700

    froala

--- libraries/component/CBookAsset/src/js/app.js
+++ libraries/component/CBookAsset/src/js/app.js
@@ -1,1 +1,25 @@
-console.log('Webpack Encore worked')
+var $ = require('jquery');
+window.jQuery = $;
+window.$ = $;
+require('popper.js')
+require('bootstrap');
+require('bootstrap-switch');
+require('codemirror');
+require('xml');
+
+import environment from './environment'
+
+window.saveBookContentChanges = () => {
+    var form = {};
+    $('.editable').each(function(i) {
+        form[`${$(this).data('name')}`] = $(this).html()
+    })
+    console.log(form)
+    $.post(environment.apiEndPoint, form)
+        .done(function(data) {
+            console.log(data)
+        })
+        .fail(function(error) {
+            console.log(error)
+        })
+}
