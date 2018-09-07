2edd413e559736bdd45811f429fe04bb96897bc1
sacachi <39644554+sacachi@users.noreply.github.com>
2018-09-07 15:37:17 +0700

    webpack admin entry

---
+++ libraries/component/CBookAsset/src/js/admin.js
@@ -0,0 +1,20 @@
+require('bootstrap-switch');
+require('codemirror');
+require('xml');
+
+import environment from './environment'
+
+window.saveBookContentChanges = () => {
+    var form = {};
+    $('.editable').each(function(i) {
+        form[`${$(this).data('name')}`] = $(this).html()
+    })
+    console.log(form)
+    $.post(environment.apiEndPoint, form)
+        .done(function(data) {
+            console.log(data)
+        })
+        .fail(function(error) {
+            console.log(error)
+        })
+}
