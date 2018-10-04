483840c586eaa0ef9ac73b5736f3e3b3fe50f301
binhle410 <binh@sunrise.vn>
2018-10-04 16:32:11 +0700

    Email of person should not be required

--- libraries/bundle/MagentaCBookAdminBundle/src/Admin/Person/PersonAdmin.php
+++ libraries/bundle/MagentaCBookAdminBundle/src/Admin/Person/PersonAdmin.php
@@ -11,43 +11,55 @@ use Sonata\AdminBundle\Form\FormMapper;
 use Sonata\AdminBundle\Form\Type\ModelType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 
-class PersonAdmin extends BaseAdmin {
-	
-	public function isGranted($name, $object = null) {
-		return $this->isAdmin();
-	}
-	
-	public function getNewInstance() {
-		/** @var Person $person */
-		$person = parent::getNewInstance();
-		if(empty($person->getUser())) {
-			$person->initiateUser(false);
-		}
-		
-		return $person;
-	}
-	
-	protected function configureFormFields(FormMapper $form) {
-		parent::configureFormFields($form);
-		$form->add('name');
-		$form->add('email');
-		$form->add('user.plainPassword', TextType::class, [ 'required' => false, 'label' => 'form.label_password' ]);
-		$form->add('user.adminOrganisations', ModelType::class, [
-			'help'     => 'This user is the root admin of the selected organisation(s)',
-			'required' => false,
-			'label'    => 'form.label_organisation',
-			'class'    => Organisation::class,
-			'multiple' => true,
-			'property' => 'name',
-			'btn_add'  => false
-		]);
-	}
-	
-	protected function configureListFields(ListMapper $list) {
-		parent::configureListFields($list);
-		$list->addIdentifier('name');
-		$list->add('email');
-	}
-	
+class PersonAdmin extends BaseAdmin
+{
+
+    public function isGranted($name, $object = null)
+    {
+        return $this->isAdmin();
+    }
+
+    public function getNewInstance()
+    {
+        /** @var Person $person */
+        $person = parent::getNewInstance();
+        if (empty($person->getUser())) {
+            $person->initiateUser(false);
+        }
+
+        return $person;
+    }
+
+    protected function configureFormFields(FormMapper $form)
+    {
+        parent::configureFormFields($form);
+        $form->add('name');
+        $form->add('email', null, ['required' => false]);
+        $form->add('user.plainPassword', TextType::class, ['required' => false, 'label' => 'form.label_password']);
+        $form->add('user.adminOrganisations', ModelType::class, [
+            'help' => 'This user is the root admin of the selected organisation(s)',
+            'required' => false,
+            'label' => 'form.label_organisation',
+            'class' => Organisation::class,
+            'multiple' => true,
+            'property' => 'name',
+            'btn_add' => false
+        ]);
+    }
+
+    protected function configureListFields(ListMapper $list)
+    {
+        parent::configureListFields($list);
+        $list->addIdentifier('name');
+        $list->add('email');
+    }
+
+    /**
+     * @param Person $object
+     */
+    public function preValidate($object)
+    {
+    }
+
 }
 
--- libraries/bundle/MagentaCBookModelBundle/src/Entity/Person/Person.php
+++ libraries/bundle/MagentaCBookModelBundle/src/Entity/Person/Person.php
@@ -16,131 +16,148 @@ use Magenta\Bundle\CBookModelBundle\Entity\User\User;
  * @ORM\Entity()
  * @ORM\Table(name="person__person")
  */
-class Person extends \Bean\Bundle\PersonBundle\Doctrine\Orm\Person implements IndividualMemberContainerInterface {
-	
-	public function __construct() {
-		parent::__construct();
-		$this->individualMembers = new ArrayCollection();
-	}
-	
-	public function initiateUser($emailRequired = true) {
-		if(empty($this->user)) {
-			$this->user = new User();
-		}
-		$this->user->setEnabled(true);
-		
-		$this->user->addRole(User::ROLE_POWER_USER);
-		if($emailRequired) {
-			if(empty($this->email)) {
-				if(empty($this->user->getEmail())) {
-					throw new \InvalidArgumentException('person email is null');
-				} else {
-					$this->email = $this->user->getEmail();
-				}
-			}
-		}
-		$username = '';
-		if( ! empty($this->givenName)) {
-			$username .= Tag::slugify(trim($this->givenName));
-			$username .= '-';
-		}
-		if( ! empty($this->familyName)) {
-			$username .= Tag::slugify(trim($this->familyName));
-			$username .= '-';
-		}
-		$now       = new \DateTime();
-		$emailName = explode('@', $this->email)[0];
-		$username  .= $emailName;
-		$username  .= $now->format('-dmY');
-		$this->user->setUsername($username);
-		$this->user->setEmail($this->email);
-		if(empty($this->user->getPlainPassword()) && empty($this->user->getPassword())) {
-			$this->user->setPlainPassword($this->email);
-		}
-		$this->user->setPerson($this);
-		
-		return $this->user;
-	}
-	
-	public function getIndividualMemberOfOrganisation(Organisation $org) {
-		/** @var IndividualMember $m */
-		foreach($this->individualMembers as $m) {
-			if($m->getOrganization() === $org) {
-				return $m;
-			}
-		}
-		
-		return null;
-	}
-	
-	/**
-	 * @var Collection
-	 * @ORM\OneToMany(targetEntity="Magenta\Bundle\CBookModelBundle\Entity\Organisation\IndividualMember", mappedBy="person")
-	 */
-	protected $individualMembers;
-	
-	public function addIndividualMember(IndividualMember $member) {
-		$this->individualMembers->add($member);
-		$member->setPerson($this);
-	}
-	
-	public function removeIndividualMember(IndividualMember $member) {
-		$this->individualMembers->removeElement($member);
-		$member->setPerson(null);
-	}
-	
-	/**
-	 * @var User|null
-	 * @ORM\OneToOne(targetEntity="Magenta\Bundle\CBookModelBundle\Entity\User\User", mappedBy="person")
-	 */
-	protected $user;
-	
-	/**
-	 * @var string|null
-	 * @ORM\Column(type="string",nullable=true)
-	 */
-	protected $idNumber;
-	
-	/**
-	 * @return User|null
-	 */
-	public function getUser(): ?User {
-		return $this->user;
-	}
-	
-	/**
-	 * @param User|null $user
-	 */
-	public function setUser(?User $user): void {
-		$this->user = $user;
-	}
-	
-	/**
-	 * @return null|string
-	 */
-	public function getIdNumber(): ?string {
-		return $this->idNumber;
-	}
-	
-	/**
-	 * @param null|string $idNumber
-	 */
-	public function setIdNumber(?string $idNumber): void {
-		$this->idNumber = $idNumber;
-	}
-	
-	/**
-	 * @return Collection
-	 */
-	public function getIndividualMembers(): Collection {
-		return $this->individualMembers;
-	}
-	
-	/**
-	 * @param Collection $individualMembers
-	 */
-	public function setIndividualMembers($individualMembers): void {
-		$this->individualMembers = $individualMembers;
-	}
-	
+class Person extends \Bean\Bundle\PersonBundle\Doctrine\Orm\Person implements IndividualMemberContainerInterface
+{
+
+    public function __construct()
+    {
+        parent::__construct();
+        $this->individualMembers = new ArrayCollection();
+    }
+
+    public function initiateUser($emailRequired = true)
+    {
+        if (empty($this->user)) {
+            $this->user = new User();
+        }
+        $this->user->setEnabled(true);
+
+        $this->user->addRole(User::ROLE_POWER_USER);
+        if ($emailRequired) {
+            if (empty($this->email)) {
+                if (empty($this->user->getEmail())) {
+//					throw new \InvalidArgumentException('person email is null');
+                    $today = new \DateTime();
+                    if (empty($this->name)) {
+                        $this->name = 'random-' . $today->getTimestamp();
+                    }
+                    $this->email = str_replace(' ', '-', $this->name) . '_' . $today->format('dmY') . '@no-email.com';
+                } else {
+                    $this->email = $this->user->getEmail();
+                }
+            }
+        }
+        $username = '';
+        if (!empty($this->givenName)) {
+            $username .= Tag::slugify(trim($this->givenName));
+            $username .= '-';
+        }
+        if (!empty($this->familyName)) {
+            $username .= Tag::slugify(trim($this->familyName));
+            $username .= '-';
+        }
+        $now = new \DateTime();
+        $emailName = explode('@', $this->email)[0];
+        $username .= $emailName;
+        $username .= $now->format('-dmY');
+        $this->user->setUsername($username);
+        $this->user->setEmail($this->email);
+        if (empty($this->user->getPlainPassword()) && empty($this->user->getPassword())) {
+            $this->user->setPlainPassword($this->email);
+        }
+        $this->user->setPerson($this);
+
+        return $this->user;
+    }
+
+    public function getIndividualMemberOfOrganisation(Organisation $org)
+    {
+        /** @var IndividualMember $m */
+        foreach ($this->individualMembers as $m) {
+            if ($m->getOrganization() === $org) {
+                return $m;
+            }
+        }
+
+        return null;
+    }
+
+    /**
+     * @var Collection
+     * @ORM\OneToMany(targetEntity="Magenta\Bundle\CBookModelBundle\Entity\Organisation\IndividualMember", mappedBy="person")
+     */
+    protected $individualMembers;
+
+    public function addIndividualMember(IndividualMember $member)
+    {
+        $this->individualMembers->add($member);
+        $member->setPerson($this);
+    }
+
+    public function removeIndividualMember(IndividualMember $member)
+    {
+        $this->individualMembers->removeElement($member);
+        $member->setPerson(null);
+    }
+
+    /**
+     * @var User|null
+     * @ORM\OneToOne(targetEntity="Magenta\Bundle\CBookModelBundle\Entity\User\User", mappedBy="person")
+     */
+    protected $user;
+
+    /**
+     * @var string|null
+     * @ORM\Column(type="string",nullable=true)
+     */
+    protected $idNumber;
+
+    /**
+     * @return User|null
+     */
+    public function getUser(): ?User
+    {
+        return $this->user;
+    }
+
+    /**
+     * @param User|null $user
+     */
+    public function setUser(?User $user): void
+    {
+        $this->user = $user;
+    }
+
+    /**
+     * @return null|string
+     */
+    public function getIdNumber(): ?string
+    {
+        return $this->idNumber;
+    }
+
+    /**
+     * @param null|string $idNumber
+     */
+    public function setIdNumber(?string $idNumber): void
+    {
+        $this->idNumber = $idNumber;
+    }
+
+    /**
+     * @return Collection
+     */
+    public function getIndividualMembers(): Collection
+    {
+        return $this->individualMembers;
+    }
+
+    /**
+     * @param Collection $individualMembers
+     */
+    public function setIndividualMembers($individualMembers): void
+    {
+        $this->individualMembers = $individualMembers;
+    }
+
 }
